de4044b3b236ad72b3ebedfbac1f6ee51c5e0fc6
Dmitry <dmasny@u4u.ru>
2020-04-19 15:33:51 +0300

    header, fonts, main styles

--- bones/page-landing.php
+++ bones/page-landing.php
@@ -15,7 +15,7 @@
 
 <?php get_header(); ?>
 
-			<div id="landing-page">
+			<div class="pageContent" id="landing-page">
                                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
                                							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
--- bones/page.php
+++ bones/page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-			<div id="content">
+			<div class="pageContent" >
 
 				<div id="inner-content" class="wrap cf">
 
